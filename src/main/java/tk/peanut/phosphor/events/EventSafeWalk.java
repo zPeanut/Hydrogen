ff4b8630f54921e4f882cb06c708995e22def2f6
zPeanut <must946@gmail.com>
2021-02-06 14:46:54 +0100

    i have no idea how to implement this lol

--- src/main/java/tk/peanut/phosphor/events/EventSafeWalk.java
+++
@@ -1,7 +0,0 @@
-package tk.peanut.phosphor.events;
-
-import com.darkmagician6.eventapi.events.Event;
-import com.darkmagician6.eventapi.events.callables.EventCancellable;
-
-public class EventSafeWalk extends EventCancellable implements Event {
-}
--- src/main/java/tk/peanut/phosphor/injection/mixins/MixinEntity.java
+++ src/main/java/tk/peanut/phosphor/injection/mixins/MixinEntity.java
@@ -1,30 +1,8 @@
 package tk.peanut.phosphor.injection.mixins;
 
-import com.darkmagician6.eventapi.EventManager;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockFence;
-import net.minecraft.block.BlockFenceGate;
-import net.minecraft.block.BlockWall;
-import net.minecraft.block.material.Material;
-import net.minecraft.crash.CrashReport;
-import net.minecraft.crash.CrashReportCategory;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.AxisAlignedBB;
-import net.minecraft.util.BlockPos;
-import net.minecraft.util.MathHelper;
-import net.minecraft.util.ReportedException;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import tk.peanut.phosphor.events.EventSafeWalk;
-
-import java.util.List;
 
 @Mixin(Entity.class)
 public class MixinEntity {
