c97c2f8512ba14ed4ad0c98c00341c0f0be0906e
zPeanut <must946@gmail.com>
2021-02-10 19:34:37 +0100

    implement eventrender3d, i think this should work

---
+++ src/main/java/tk/peanut/hydrogen/events/EventRender3D.java
@@ -0,0 +1,21 @@
+package tk.peanut.hydrogen.events;
+
+import com.darkmagician6.eventapi.events.callables.EventCancellable;
+
+/**
+ * Created by peanut on 10/02/2021
+ */
+public class EventRender3D extends EventCancellable {
+
+    public static float partialTicks;
+
+    public EventRender3D(float partialTicks)
+    {
+        this.partialTicks = partialTicks;
+    }
+
+    public float getPartialTicks()
+    {
+        return partialTicks;
+    }
+}
