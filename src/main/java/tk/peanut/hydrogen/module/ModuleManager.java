eb10b5458fef5fa772c78ee9c2f421928fa08801
profkambing <75421414+fawwaz1234@users.noreply.github.com>
2021-02-25 14:26:40 +0800

    Update ModuleManager.java

--- src/main/java/tk/peanut/hydrogen/module/ModuleManager.java
+++ src/main/java/tk/peanut/hydrogen/module/ModuleManager.java
@@ -55,6 +55,8 @@ public class ModuleManager {
         add(new Velocity());
         add(new Flight());
         add(new ItemESP());
+        add(new Speed());
+        add(new NoFall());
 
         // alphabetically sort modules in clickgui
 
