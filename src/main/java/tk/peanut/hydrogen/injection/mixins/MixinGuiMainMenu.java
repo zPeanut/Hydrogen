c1190ec438075c668013b30f7b8dd9ec3dcb4de3
zPeanut <must946@gmail.com>
2021-02-26 14:44:03 +0100

    refresh mainmenu

--- src/main/java/tk/peanut/hydrogen/injection/mixins/MixinGuiMainMenu.java
+++ src/main/java/tk/peanut/hydrogen/injection/mixins/MixinGuiMainMenu.java
@@ -2,33 +2,22 @@ package tk.peanut.hydrogen.injection.mixins;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiButtonLanguage;
 import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.texture.DynamicTexture;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.realms.RealmsBridge;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.ISaveFormat;
-import net.minecraft.world.storage.WorldInfo;
-import net.minecraftforge.common.ForgeVersion;
-import net.minecraftforge.fml.common.Loader;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Inject;
-import tk.peanut.hydrogen.Hydrogen;
-import tk.peanut.hydrogen.utils.ExpandButton;
-import tk.peanut.hydrogen.utils.FontHelper;
-import tk.peanut.hydrogen.utils.ParticleGenerator;
+import tk.peanut.hydrogen.ui.mainmenu.utils.ExpandButton;
 import tk.peanut.hydrogen.utils.Utils;
 
-import java.awt.*;
 import java.util.Calendar;
 import java.util.Date;
 
