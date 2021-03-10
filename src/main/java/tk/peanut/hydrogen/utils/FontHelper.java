ca4c060334e5421681360e0b7c9cf57bb22bf38a
zPeanut <must946@gmail.com>
2021-03-10 15:11:01 +0100

    add new fonts

--- src/main/java/tk/peanut/hydrogen/utils/FontHelper.java
+++ src/main/java/tk/peanut/hydrogen/utils/FontHelper.java
@@ -12,6 +12,8 @@ public class FontHelper {
     public static H2FontRenderer fontnormal;
     public static H2FontRenderer fontlarge;
 
+    public static H2FontRenderer cfontnormal;
+    public static H2FontRenderer cfontlarge;
 
 
 
@@ -20,6 +22,9 @@ public class FontHelper {
 
             fontlarge = new H2FontRenderer(new Font("Verdana", Font.PLAIN, 40), true, 8);
             fontnormal = new H2FontRenderer(new Font("Verdana", Font.PLAIN, 32), true, 8);
+
+            cfontlarge = new H2FontRenderer(new Font("Comfortaa", Font.PLAIN, 40), true, 8);
+            cfontnormal = new H2FontRenderer(new Font("Comfortaa", Font.PLAIN, 32), true, 8);
 
         } catch (Exception exception) {}
     }
