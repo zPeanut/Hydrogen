5c789df3cc8d0b39bd2865f26e9168f67565e3c1
zPeanut <must946@gmail.com>
2021-03-12 18:32:05 +0100

    fix font

--- src/main/java/tk/peanut/hydrogen/utils/FontHelper.java
+++ src/main/java/tk/peanut/hydrogen/utils/FontHelper.java
@@ -34,11 +34,11 @@ public class FontHelper {
 
             verdana = new H2FontRenderer(new Font("Verdana", Font.PLAIN, 32), true, 8);
 
-            InputStream stream = FontHelper.class.getClass().getResourceAsStream("/assets/hydrogen/SF-UI-Display-Light.otf");
+            InputStream stream = FontHelper.class.getClass().getResourceAsStream("/assets/hydrogen/SF-UI-Display-Light.ttf");
             Font font = Font.createFont(Font.TRUETYPE_FONT, stream).deriveFont(36f);
             sf_l = new H2FontRenderer(font, true, 8);
 
-            InputStream stream33 = FontHelper.class.getClass().getResourceAsStream("/assets/hydrogen/SF-UI-Display-Light.otf");
+            InputStream stream33 = FontHelper.class.getClass().getResourceAsStream("/assets/hydrogen/SF-UI-Display-Light.ttf");
             Font font33 = Font.createFont(Font.TRUETYPE_FONT, stream33).deriveFont(72f);
             sf_l2 = new H2FontRenderer(font33, true, 8);
 
@@ -49,10 +49,6 @@ public class FontHelper {
             InputStream stream3 = FontHelper.class.getClass().getResourceAsStream("/assets/hydrogen/Comfortaa-Light.ttf");
             Font font3 = Font.createFont(Font.TRUETYPE_FONT, stream3).deriveFont(36f);
             comfortaa_l = new H2FontRenderer(font3, true, 8);
-
-
-
-
 
         } catch (Exception exception) {}
     }
