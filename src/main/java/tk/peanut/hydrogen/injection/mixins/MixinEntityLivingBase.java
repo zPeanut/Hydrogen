2064479c11fb6d123e88226c00b2420e07d2590e
zPeanut <must946@gmail.com>
2021-04-06 12:31:33 +0200

    remove unused shadow fields

--- src/main/java/tk/peanut/hydrogen/injection/mixins/MixinEntityLivingBase.java
+++ src/main/java/tk/peanut/hydrogen/injection/mixins/MixinEntityLivingBase.java
@@ -5,8 +5,12 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.util.BlockPos;
+import net.minecraftforge.common.ForgeHooks;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 /**
  * Created by peanut on 07/02/2021
@@ -20,6 +24,15 @@ public abstract class MixinEntityLivingBase extends MixinEntity {
 
     @Shadow
     public abstract IAttributeInstance getEntityAttribute(IAttribute attribute);
+
+    @Shadow
+    private EntityLivingBase entityLivingToAttack;
+
+    @Shadow
+    private int revengeTimer;
+
+    @Shadow
+    public int ticksExisted;
 
 
 }
