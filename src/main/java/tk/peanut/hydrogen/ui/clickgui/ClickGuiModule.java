e7caffc54dc2a0ed5e39f358a060cb8644e3a259
zPeanut <must946@gmail.com>
2021-06-24 14:01:54 +0200

    change default clickgui bind from lshift to lcontrol

--- src/main/java/tk/peanut/hydrogen/ui/clickgui/ClickGuiModule.java
+++ src/main/java/tk/peanut/hydrogen/ui/clickgui/ClickGuiModule.java
@@ -22,7 +22,7 @@ public class ClickGuiModule extends Module {
     public ClickGui clickgui;
 
     public ClickGuiModule() {
-        super(Keyboard.KEY_LSHIFT, colorRender);
+        super(Keyboard.KEY_LCONTROL, colorRender);
 
         ArrayList<String> font = new ArrayList<>();
         font.add("TTF");
