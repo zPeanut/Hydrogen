62321056285d8b8c3500dbd61df1c6448c9dafef
zPeanut <must946@gmail.com>
2021-07-14 12:01:46 +0200

    add thealtening auth lib

---
+++ src/main/java/com/thealtening/auth/Pair.java
@@ -0,0 +1,22 @@
+package com.thealtening.auth;
+
+public class Pair<K, V> {
+
+	private final K key;
+
+	private final V value;
+
+	public Pair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public K getKey() {
+		return this.key;
+	}
+
+	public V getValue() {
+		return this.value;
+	}
+
+}
---
+++ src/main/java/com/thealtening/auth/UserSkin.java
@@ -0,0 +1,15 @@
+package com.thealtening.auth;
+
+public class UserSkin {
+
+	private final AlteningAlt account;
+
+	public UserSkin(AlteningAlt account) {
+		this.account = account;
+	}
+
+	public AlteningAlt getAccount() {
+		return account;
+	}
+
+}
