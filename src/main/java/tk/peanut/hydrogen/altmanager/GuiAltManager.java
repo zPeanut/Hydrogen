84099a47f719ee45f718f42a6920e4f0c6a25aaf
zPeanut <must946@gmail.com>
2021-07-14 12:01:53 +0200

    add alt manager

---
+++ src/main/java/tk/peanut/hydrogen/altmanager/GuiAltManager.java
@@ -0,0 +1,118 @@
+package tk.peanut.hydrogen.altmanager;
+
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.nio.charset.MalformedInputException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+import com.thealtening.auth.AltService;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.ScaledResolution;
+import tk.peanut.hydrogen.Hydrogen;
+import tk.peanut.hydrogen.altmanager.account.Account;
+import tk.peanut.hydrogen.altmanager.components.GuiAccountList;
+import tk.peanut.hydrogen.altmanager.impl.GuiAddAlt;
+import tk.peanut.hydrogen.altmanager.impl.GuiAltLogin;
+import tk.peanut.hydrogen.altmanager.impl.GuiAlteningLogin;
+import tk.peanut.hydrogen.altmanager.thread.AccountLoginThread;
+
+public class GuiAltManager extends GuiScreen {
+
+	public static GuiAltManager INSTANCE;
+
+	private GuiAccountList accountList;
+
+	public Account currentAccount;
+
+	public AccountLoginThread loginThread;
+
+	private String status = "§eWaiting for login...";
+
+	public GuiAltManager() {
+		INSTANCE = this;
+	}
+
+	public void initGui() {
+		this.accountList = new GuiAccountList(this);
+		this.accountList.registerScrollButtons(7, 8);
+		this.accountList.elementClicked(-1, false, 0, 0);
+		this.buttonList.add(new GuiButton(0, this.width / 2 + 54, this.height - 24, 100, 20, "Cancel"));
+		this.buttonList.add(new GuiButton(1, this.width / 2 - 154, this.height - 48, 100, 20, "Login"));
+		this.buttonList.add(new GuiButton(2, this.width / 2 - 50, this.height - 24, 100, 20, "Remove"));
+		this.buttonList.add(new GuiButton(3, this.width / 2 - 154, this.height - 24, 100, 20, "Add"));
+		this.buttonList.add(new GuiButton(4, this.width / 2 - 50, this.height - 48, 100, 20, "Direct Login"));
+		this.buttonList.add(new GuiButton(6, this.width / 2 + 4 + 50, this.height - 48, 100, 20, "TheAltening"));
+	}
+
+	public void drawScreen(int p_drawScreen_1_, int p_drawScreen_2_, float p_drawScreen_3_) {
+		ScaledResolution scaledResolution = new ScaledResolution(this.mc);
+		drawDefaultBackground();
+		this.accountList.drawScreen(p_drawScreen_1_, p_drawScreen_2_, p_drawScreen_3_);
+		super.drawScreen(p_drawScreen_1_, p_drawScreen_2_, p_drawScreen_3_);
+
+		if (this.loginThread != null)
+			status = this.loginThread.getStatus();
+		if (Hydrogen.getClient().altService.getCurrentService().equals(AltService.EnumAltService.MOJANG)) {
+			drawCenteredString(this.mc.fontRendererObj, status, scaledResolution.getScaledWidth() / 2, 6, -3158065);
+			drawCenteredString(this.mc.fontRendererObj, "Accounts: " + Hydrogen.getClient().accountManager.getAccounts().size(), this.width / 2, 20, -1);
+		} else
+			drawCenteredString(this.mc.fontRendererObj, "Logged in with TheAltening §7(" + this.mc.getSession().getUsername() + ")", this.width / 2, 20, -1);
+	}
+
+	public void handleMouseInput() throws IOException {
+		super.handleMouseInput();
+		this.accountList.handleMouseInput();
+	}
+
+	protected void actionPerformed(GuiButton button) throws IOException {
+		ArrayList<Account> registry;
+		Random random;
+		Account randomAlt;
+		JFrame frame;
+		JFileChooser chooser;
+		int returnVal;
+		switch (button.id) {
+		case 0:
+			if (this.loginThread == null || !this.loginThread.getStatus().contains("Logging in"))
+				this.mc.displayGuiScreen((GuiScreen) new GuiMainMenu());
+			break;
+		case 1:
+			if (this.accountList.selected == -1)
+				return;
+			this.loginThread = new AccountLoginThread(this.accountList.getSelectedAccount().getEmail(), this.accountList.getSelectedAccount().getPassword());
+			this.loginThread.start();
+			break;
+		case 2:
+			this.accountList.removeSelected();
+			this.accountList.selected = -1;
+			break;
+		case 3:
+			if (this.loginThread != null)
+				this.loginThread = null;
+			this.mc.displayGuiScreen((GuiScreen) new GuiAddAlt(this));
+			break;
+		case 4:
+			if (this.loginThread != null)
+				this.loginThread = null;
+			this.mc.displayGuiScreen((GuiScreen) new GuiAltLogin(this));
+			break;
+		case 6:
+			this.mc.displayGuiScreen((GuiScreen) new GuiAlteningLogin(this));
+			break;
+		}
+	}
+
+	public void login(Account account) {
+		this.loginThread = new AccountLoginThread(account.getEmail(), account.getPassword());
+		this.loginThread.start();
+	}
+
+}
---
+++ src/main/java/tk/peanut/hydrogen/altmanager/impl/AccountImport.java
@@ -0,0 +1,51 @@
+package tk.peanut.hydrogen.altmanager.impl;
+
+import tk.peanut.hydrogen.Hydrogen;
+import tk.peanut.hydrogen.altmanager.account.Account;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class AccountImport extends JPanel implements ActionListener {
+	private static final long serialVersionUID = 1L;
+
+	public JButton openButton;
+
+	private JFileChooser fc;
+
+	public AccountImport() {
+		this.fc = new JFileChooser();
+		this.fc.setFileFilter(new FileNameExtensionFilter("Text Files", new String[] { "txt" }));
+		this.openButton = new JButton("Open a File...");
+		this.openButton.addActionListener(this);
+		add(this.openButton);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == this.openButton) {
+			int returnVal = this.fc.showOpenDialog(this);
+			if (returnVal == 0)
+				try (BufferedReader bufferedReader = new BufferedReader(new FileReader(this.fc.getSelectedFile()))) {
+					String line;
+					while ((line = bufferedReader.readLine()) != null) {
+						String[] arguments = line.split(":");
+						for (int i = 0; i < 2; i++)
+							arguments[i].replace(" ", "");
+						Hydrogen.getClient().accountManager.getAccounts().add(new Account(arguments[0], arguments[1], ""));
+					}
+					Hydrogen.getClient().accountManager.save();
+				} catch (Exception ex) {
+					JOptionPane.showMessageDialog(null, "An error happened.", "ERROR", 1);
+				}
+		}
+	}
+
+}
