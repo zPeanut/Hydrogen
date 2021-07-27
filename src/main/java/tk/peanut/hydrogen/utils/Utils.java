1a99b3325ed082ff003c313f95f713abc2b0ef27
zPeanut <must946@gmail.com>
2021-07-27 22:37:55 +0200

    add new utils for bowaimbot

--- src/main/java/tk/peanut/hydrogen/utils/Utils.java
+++ src/main/java/tk/peanut/hydrogen/utils/Utils.java
@@ -861,4 +861,44 @@ public class Utils {
             scaleFactor++;
         return new Point(Mouse.getX() / scaleFactor, minecraft.displayHeight / scaleFactor - Mouse.getY() / scaleFactor - 1);
     }
+
+    public static float[] getRotationFromPosition(double x, double z, double y) {
+        double xDiff = x - Minecraft.getMinecraft().thePlayer.posX;
+        double zDiff = z - Minecraft.getMinecraft().thePlayer.posZ;
+        double yDiff = y - Minecraft.getMinecraft().thePlayer.posY - 1.2;
+
+        double dist = MathHelper.sqrt_double(xDiff * xDiff + zDiff * zDiff);
+        float yaw = (float) (Math.atan2(zDiff, xDiff) * 180.0D / 3.141592653589793D) - 90.0F;
+        float pitch = (float) -(Math.atan2(yDiff, dist) * 180.0D / 3.141592653589793D);
+        return new float[]{yaw, pitch};
+    }
+
+    public static float[] getRotations(EntityLivingBase ent) {
+        double x = ent.posX;
+        double z = ent.posZ;
+        double y = ent.posY + ent.getEyeHeight() / 2.0F;
+        return getRotationFromPosition(x, z, y);
+    }
+
+    public static float[] getBowAngles(final Entity entity) {
+        final double xDelta = (entity.posX - entity.lastTickPosX) * 0.4;
+        final double zDelta = (entity.posZ - entity.lastTickPosZ) * 0.4;
+        double d = Minecraft.getMinecraft().thePlayer.getDistanceToEntity(entity);
+        d -= d % 0.8;
+        double xMulti = 1.0;
+        double zMulti = 1.0;
+        final boolean sprint = entity.isSprinting();
+        xMulti = d / 0.8 * xDelta * (sprint ? 1.25 : 1.0);
+        zMulti = d / 0.8 * zDelta * (sprint ? 1.25 : 1.0);
+        final double x = entity.posX + xMulti - Minecraft.getMinecraft().thePlayer.posX;
+        final double z = entity.posZ + zMulti - Minecraft.getMinecraft().thePlayer.posZ;
+        final double y = Minecraft.getMinecraft().thePlayer.posY + Minecraft.getMinecraft().thePlayer.getEyeHeight() - (entity.posY + entity.getEyeHeight());
+        final double dist = Minecraft.getMinecraft().thePlayer.getDistanceToEntity(entity);
+        final float yaw = (float) Math.toDegrees(Math.atan2(z, x)) - 90.0f;
+        double d1 = MathHelper.sqrt_double(x * x + z * z);
+        final float pitch =  (float) - (Math.atan2(y, d1) * 180.0D / Math.PI) + (float)dist*0.11f;
+
+        return new float[]{yaw, -pitch};
+    }
+
 }
