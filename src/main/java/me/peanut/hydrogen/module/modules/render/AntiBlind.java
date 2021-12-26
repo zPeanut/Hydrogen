bb72d0852d352519e49456367c7441bca9520d5b
zPeanut <must946@gmail.com>
2021-12-26 18:36:33 +0100

    add antiblind

---
+++ src/main/java/me/peanut/hydrogen/module/modules/render/AntiBlind.java
@@ -0,0 +1,23 @@
+package me.peanut.hydrogen.module.modules.render;
+
+import me.peanut.hydrogen.module.Category;
+import me.peanut.hydrogen.module.Info;
+import me.peanut.hydrogen.module.Module;
+import me.peanut.hydrogen.settings.Setting;
+
+/**
+ * Created by peanut on 25/12/2021
+ */
+@Info(name = "AntiBlind", description = "Removes any visually impairing effects", category = Category.Render)
+public class AntiBlind extends Module {
+
+    public AntiBlind() {
+        super(0x00);
+
+        addSetting(new Setting("Pumpkin", this, true));
+        addSetting(new Setting("Fire", this, true));
+        addSetting(new Setting("Potion", this, true));
+    }
+
+
+}
