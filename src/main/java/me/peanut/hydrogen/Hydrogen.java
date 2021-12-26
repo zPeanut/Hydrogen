1a33ba2ff87cae8cc50f048ef4bf813baeb169ec
zPeanut <must946@gmail.com>
2021-12-26 22:45:12 +0100

    push to 1.11 prerelease

--- src/main/java/me/peanut/hydrogen/Hydrogen.java
+++ src/main/java/me/peanut/hydrogen/Hydrogen.java
@@ -31,8 +31,8 @@ public class Hydrogen {
     public static final String devs = "zPeanut & UltramoxX";
     public static final String prefix = "§7[§9" + name + "§7]";
 
-    public static final String version = "1.11 Dev";
-    public static final String semantic_version = "1.11.0-dev";
+    public static final String version = "1.11 Prerelease";
+    public static final String semantic_version = "1.11.0-pre";
 
     public static final String github = "https://github.com/zpeanut/hydrogen/";
     public static final String release = github + "releases/";
