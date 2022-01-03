d61719a275218ca3b92312f674b569cf43ef3fea
zPeanut <must946@gmail.com>
2022-01-03 23:35:38 +0100

    push to 1.12 dev

--- src/main/java/me/peanut/hydrogen/Hydrogen.java
+++ src/main/java/me/peanut/hydrogen/Hydrogen.java
@@ -30,8 +30,8 @@ public class Hydrogen {
     public static final String devs = "zPeanut & UltramoxX";
     public static final String prefix = "§7[§9" + name + "§7]";
 
-    public static String version = "1.11.3";
-    public static final String semantic_version = "1.11.3";
+    public static String version = "1.12 Dev";
+    public static final String semantic_version = "1.12.0-dev";
 
     public static final String github = "https://github.com/zpeanut/hydrogen/";
     public static final String release = github + "releases/";
@@ -54,7 +54,7 @@ public class Hydrogen {
     public boolean outdated;
     public boolean panic;
     public boolean firstStart;
-    public boolean isStableBuild = true;
+    public boolean isStableBuild = false;
     public String newversion;
 
     public Hydrogen() {
@@ -70,6 +70,8 @@ public class Hydrogen {
             directory.mkdir();
         }
         if(!isStableBuild) {
+            // get commit dates
+            Utils.getCurrentCommitDate();
             // add git commit hash to version
             version += String.format(" §7| %s", Utils.getCurrentCommitHash());
         }
