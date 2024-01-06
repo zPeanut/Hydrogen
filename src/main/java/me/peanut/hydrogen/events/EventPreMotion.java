45635d396d343ada027dfd574d91f8707d5d86d7
zPeanut <must946@gmail.com>
2024-01-06 14:31:23 +0100

    delete old stuff

--- src/main/java/me/peanut/hydrogen/events/EventPreMotion.java
+++
@@ -1,6 +0,0 @@
-package me.peanut.hydrogen.events;
-
-import com.darkmagician6.eventapi.events.Event;
-
-public class EventPreMotion implements Event {
-}
--- src/main/java/me/peanut/hydrogen/events/EventRender2D.java
+++
@@ -1,7 +0,0 @@
-package me.peanut.hydrogen.events;
-
-import com.darkmagician6.eventapi.events.Event;
-
-public class EventRender2D implements Event {
-
-}
